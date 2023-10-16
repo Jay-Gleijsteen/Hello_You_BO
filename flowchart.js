ca052fdb0ce1db2d351a8d916aac5cbeb6e4fefc
jay gleijsteen <36629@ma-web.nl>
2023-10-16 14:31:53 +0200

    commit 1

---
+++ flowchart.js
@@ -0,0 +1,42 @@
+let readlineSync = require('readline-sync');
+
+let begin = readlineSync.question("wil je beginnen?");
+let einde1 = "lui einde"
+let einde2 = "vermoord einde"
+let einde3 = "ontsnapt einde"
+let einde4 = "goed einde"
+let einde5 = "je kan niet verstoppen in de muren???"
+let text1 = "je bent alleen thuis in je bed savonds laat. je hoort de deurbel gaan.je....."
+let text2 = "je gaat naar de deur. je kijkt door het raampje van de deur en ziet een man. de man screeuwt:DOE OPEN.hij heeft je gezien. je twijfelt of je de deur open wil doen. je besslist om...."
+let text3 = "je blijft in bed liggen. ga naar einde 1"
+let text4 = "je besslist de deur niet open te doen. de man word aggresiever: DOE DIE DEUR OPEN. NU!je word bang en gaat schuilen. je hoort even niks meer. nu hoor je de deurklink van je achterdeur open gaan. je was vergeten hem opslot te doen toen je ging slapen. de man zegt: ik weet dat je hier ben rot joch. je weet dat je niet goed genoeg verstopt zit. hij zal je vinden. je besslist om ergens anders te verstoppen. je "
+let text5 = "je doet de deur open. de man zegt:dankje dat je de deur open deed. de man: nu heb ik je uiteindelijk te pakken. hij pakt een mes een steekt hem door je keel. je bloed dood. ga naar einde 2"
+let text6 = "je gaat naar de kelder toe. je rent snel de kelder in en doet de kelder op slot.je hoort de man zeggen: ik zal je vinden. nu zit je vast in de kelder. het is een paar minuten geweest(voor jou gevoel).het lijkt nu bijna stil. je..."
+let text7 = "blijft wachten in de hoop dat de man weg gaat of dat er hulp komt. je begint honger te krijgen. er is niks in de kelder. je weet dat er eten boven is. je..."
+let text8 = "je doet de kelder deur open. je ziet niemand. je bent veilig. of tenminsten. dat dacht je. de man komt van achter de kast vandaag en neemt je in een greep: ik wist dat ik je zou pakken rat. hij pakt zijn mes en snijd je keel door"
+let text9 = "je verstopt in de muren. ga naar einde 5"
+let text10 = "je blijft in de keleder ondanks de enorme honger. je vraagt je af hoelang je je nog zal bevinden in deze kelder. plotseling hoor je een geluid. het is een rat. de rat snuffelt rustig rond terwijl jij hem aanstaart. vers eten."
+let text11 = "je staart naar de rat terwijl hij steeds dichterbij komt. je wilt hem laten leven maar de honger neemt over"
+let text12 = "zodra de rat dichtbij je is pak je je kans en eet je hem op. lekker. je honger neemt af. je kan nu langer in de kelder blijven. ineens begin je je raar te voelen. je maag brand. je armen schudden. en je ogen worden bloedrood. er was iets mis met die muis. de honger neemt snel toe. je wilt meer. MEER. je armen willen meer slachtoffers maken. maar er zijn geen ratten meer. zover jij weet is er nog 1 ander organisme in het huis. je.."
+let text13 = ""
+let text14 = ""
+let text15 = ""
+let text16 = ""
+let text17 = ""
+let text18 = ""
+let text19 = ""
+let text20 = ""
+let text21 = ""
+let text22 = ""
+let text23 = ""
+let text24 = ""
+console.log(text1)
+let vraag1 = readlineSync.question("A: gaat naar de deur\nB:je blijft liggen ");{
+    if (vraag1 = "A"){
+        console.log("ga naar text 2")}
+
+        else (vraag1 == "B");{
+            console.log("ga naar text 3")
+    }
+    }
+
